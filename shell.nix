1167856fac4675052f947ce357bf33de7343163b
Ben Ford <ben@perurbis.com>
2015-04-18 11:10:33 +0100

    Add errors pacage

--- shell.nix
+++ shell.nix
@@ -1,19 +1,22 @@
 with (import <nixpkgs> {}).pkgs;
 let pkg = haskellngPackages.callPackage
-            ({ mkDerivation, aeson, base, bytestring, either, free, hedis, lens
-             , pipes, pipes-concurrency, stdenv, tasty, tasty-hspec
-             , tasty-quickcheck, text, transformers, uuid
+            ({ mkDerivation, aeson, attoparsec, base, bytestring, containers
+             , either, errors, free, hedis, lens, mmorph, mtl, pipes
+             , pipes-concurrency, stdenv, tasty, tasty-hspec, tasty-quickcheck
+             , text, transformers, uuid
              }:
              mkDerivation {
                pname = "cdo";
                version = "0.1.0.0";
                src = ./.;
                buildDepends = [
-                 aeson base bytestring either free hedis lens pipes
-                 pipes-concurrency text transformers uuid
+                 aeson attoparsec base bytestring containers either errors free
+                 hedis lens mmorph mtl pipes pipes-concurrency text transformers
+                 uuid
                ];
                testDepends = [
-                 base bytestring tasty tasty-hspec tasty-quickcheck
+                 base bytestring containers mmorph tasty tasty-hspec
+                 tasty-quickcheck
                ];
                homepage = "https://github.com/boothead/cdo";
                description = "Free Monad based CQRS";
